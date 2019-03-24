dba29ff606f800b9ff378f712878547650c0ea64
Robert Burek <robertburek@wp.pl>
2019-03-24 13:33:00 +0100

    Drugi commit - dodałem nową linie informacjną

--- src/RobertBurek.java
+++ src/RobertBurek.java
@@ -4,5 +4,6 @@
 public class RobertBurek {
     public static void main (String[] args){
         System.out.println("Robert Burek");
+        System.out.println("Nowa linia w projekcie.");
     }
 }
