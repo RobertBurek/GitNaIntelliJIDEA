6aba268bb16cc83184b51b39de17d264efec1f40
RobertBurek <47274949+RobertBurek@users.noreply.github.com>
2019-03-24 16:22:11 +0100

    Usunąłem niepotrzebną linie - trzecią

--- src/RobertBurek.java
+++ src/RobertBurek.java
@@ -5,6 +5,5 @@ public class RobertBurek {
     public static void main (String[] args){
         System.out.println("Robert Burek");
         System.out.println("Nowa linia w projekcie.");
-        System.out.println("Nowa trzecia linia - nie potrzebna!!!");
     }
 }
