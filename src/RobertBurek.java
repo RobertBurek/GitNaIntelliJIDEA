7e4f7dce2700e94f76ff642d88a88e9a0b5cdd05
Robert Burek <robertburek@wp.pl>
2019-03-24 16:57:27 +0100

    Ostateczne zmiany dotyczące metody main

--- src/RobertBurek.java
+++ src/RobertBurek.java
@@ -1,13 +1,33 @@
+import java.util.ArrayList;
+
 /**
  * Created by Robert Burek
  */
 public class RobertBurek {
+
+    public static void wypiszZdania(ArrayList listaZdan){
+        int i = 1;
+        for(Object s : listaZdan){
+            System.out.print(i + " - ");
+            System.out.println(s);
+            i++;
+        }
+    }
+
     public static void main (String[] args){
-        System.out.println("Robert Burek");
-        System.out.println("Robert Burek");
-        System.out.println("Jednak dodałem jeszcze inną linie w tym miejscu");
-        System.out.println("Jednak dodałem jeszcze inną linie w tym miejscu");
-        System.out.println("Nowa linia w projekcie.");
-        System.out.println("Nowa linia w projekcie.");
+//        System.out.println("Robert Burek");
+//        System.out.println("Robert Burek");
+//        System.out.println("Jednak dodałem jeszcze inną linie w tym miejscu");
+//        System.out.println("Jednak dodałem jeszcze inną linie w tym miejscu");
+//        System.out.println("Nowa linia w projekcie.");
+//        System.out.println("Nowa linia w projekcie.");
+
+        ArrayList<String> listaZdan = new ArrayList<>();
+
+        listaZdan.add("Robert Burek");
+        listaZdan.add("Jednak dodałem jeszcze inną linie w tym miejscu");
+        listaZdan.add("Nowa linia w projekcie.");
+
+        wypiszZdania(listaZdan);
     }
 }
