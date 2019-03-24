94141d08528740126b5839c638cf9d0dc946067a
RobertBurek <47274949+RobertBurek@users.noreply.github.com>
2019-03-24 16:46:04 +0100

    To są nowe zmiany, totalnie nie konieczne.

--- src/RobertBurek.java
+++ src/RobertBurek.java
@@ -4,7 +4,10 @@
 public class RobertBurek {
     public static void main (String[] args){
         System.out.println("Robert Burek");
+        System.out.println("Robert Burek");
         System.out.println("Jednak dodałem jeszcze inną linie w tym miejscu");
+        System.out.println("Jednak dodałem jeszcze inną linie w tym miejscu");
+        System.out.println("Nowa linia w projekcie.");
         System.out.println("Nowa linia w projekcie.");
     }
 }
