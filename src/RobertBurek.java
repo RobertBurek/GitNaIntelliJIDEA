d459c15f8f47b0f23793d5b4682cf9dc5aa0092d
Robert Burek <robertburek@wp.pl>
2019-03-24 19:07:58 +0100

    GitNaIntelliJIDEA-1 Dodać funcjonalność losowego wyświetlania zdań

--- src/RobertBurek.java
+++ src/RobertBurek.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+import java.util.*;
 
 /**
  * Created by Robert Burek
@@ -14,6 +14,23 @@ public class RobertBurek {
         }
     }
 
+    public static Set<Integer> sprawdzamListe = new HashSet<>();
+
+    public static void wypiszZdaniaLosowo(ArrayList listaZdan){
+        int i = 0;
+        Random iLosowe = new Random();
+        int z;
+        String[] listaPierwotna = new String[3];
+        for(Object s : listaZdan) {
+            listaPierwotna[i] = s.toString();
+            i++;
+        }
+        for (int j = 0; j < 3; j++){
+            while (!sprawdzamListe.add(z = iLosowe.nextInt(3))){}
+            System.out.println((z + 1) + " - " + listaPierwotna[z]);
+        }
+    }
+
     public static void main (String[] args){
 
         ArrayList<String> listaZdan = new ArrayList<>();
@@ -23,5 +40,7 @@ public class RobertBurek {
         listaZdan.add("Nowa linia w projekcie.");
 
         wypiszZdania(listaZdan);
+        System.out.println("---------------LOSOWO----------------------");
+        wypiszZdaniaLosowo(listaZdan);
     }
 }
