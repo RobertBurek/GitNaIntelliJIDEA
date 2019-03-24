998f80d49dd74d9400dab30ce4bb9046c91e5073
Robert Burek <robertburek@wp.pl>
2019-03-24 17:28:45 +0100

    GitNaIntelliJIDEA-2 Uporządkować kod

--- src/RobertBurek.java
+++ src/RobertBurek.java
@@ -15,12 +15,6 @@ public class RobertBurek {
     }
 
     public static void main (String[] args){
-//        System.out.println("Robert Burek");
-//        System.out.println("Robert Burek");
-//        System.out.println("Jednak dodałem jeszcze inną linie w tym miejscu");
-//        System.out.println("Jednak dodałem jeszcze inną linie w tym miejscu");
-//        System.out.println("Nowa linia w projekcie.");
-//        System.out.println("Nowa linia w projekcie.");
 
         ArrayList<String> listaZdan = new ArrayList<>();
 
