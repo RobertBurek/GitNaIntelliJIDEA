5f53e242a55658a7c2a84c6d2d2749fb3c48e082
RobertBurek <47274949+RobertBurek@users.noreply.github.com>
2019-03-24 16:25:18 +0100

    Dodałem inną linię w innym miejscu.

--- src/RobertBurek.java
+++ src/RobertBurek.java
@@ -4,6 +4,7 @@
 public class RobertBurek {
     public static void main (String[] args){
         System.out.println("Robert Burek");
+        System.out.println("Jednak dodałem jeszcze inną linie w tym miejscu");
         System.out.println("Nowa linia w projekcie.");
     }
 }
